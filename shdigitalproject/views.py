09b9c50842604d4399e6c47b9b9ada9d493240f8
Sandro Huber <sandro@sh-digital.ch>
2020-07-24 23:42:58 +0200

    2nd commit

--- shdigitalproject/views.py
+++ shdigitalproject/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
 def home(request):
-	return render(request, 'home.html', {})
+	if request.method == "POST":
+		message_name = request.POST['message-name']
+		message_email = request.POST['message-email']
+		message_subject = request.POST['message-subject']
+		message_content = request.POST['message-content']
+		return render (request, 'home.html', {'message_content': message_content})
+
+	else:
+		return render (request, 'home.html',{})
